9039b89e20cc7ebfb596f21305021a7b469a5356
charlesmarseille <charles_marseille@hotmail.com>
2024-12-12 16:06:11 -0500

    corrected scripts/commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,5 @@
 #Source the env file so the variables are accessible in the container
+
 . /mnt/alinea_vol/config.env
 
 echo "!!!cd to mnt/alinea_vol"
@@ -8,5 +9,6 @@ echo "downloading data from sentinel1 for zone : ${ZONE}"
 echo "Shapefile path: '${FOLDERPATH}/${SHAPEFILE_NAME}'"
 echo "Dates: ${CAPTURE_DATE}"
 python3 /root/ALINEA/scripts/download_sentinel1_from_extent.py $ZONE "/mnt/alinea_vol/${SHAPEFILE_NAME}" $CAPTURE_DATE
+#python3 /root/ALINEA/scripts/process_sentinel.py --folder-name "/mnt/alinea_vol/"
 
 echo "done."
--- scripts/commands.sh
+++ scripts/commands.sh
@@ -2,8 +2,8 @@ echo "cd to mnt/alinea_vol"
 cd /mnt/alinea_vol/
 
 echo "downloading data from sentinel1 for zone : $ZONE"
-echo "Shapefile path: '${FOLDERPATH}\\${SHAPEFILE_NAME}'"
+echo "Shapefile path: '${FOLDERPATH}/${SHAPEFILE_NAME}'"
 echo "Dates: $CAPTURE_DATE"
-python3 /root/ALINEA/scripts/download_sentinel1_from_extent.py $ZONE "${FOLDERPATH}\\${SHAPEFILE_NAME}" $CAPTURE_DATE
+python3 /root/ALINEA/scripts/download_sentinel1_from_extent.py $ZONE "${FOLDERPATH}/${SHAPEFILE_NAME}" $CAPTURE_DATE
 
 echo "done."
