0dc8003229c3a078a8170b85661629095d172873
charlesmarseille <charles_marseille@hotmail.com>
2024-12-12 19:58:57 -0500

    corrected scripts/commands.sh

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -1,9 +1,14 @@
-echo "cd to mnt/alinea_vol"
+#Source the env file so the variables are accessible in the container
+
+. /mnt/alinea_vol/config.env
+
+echo "!!!cd to mnt/alinea_vol"
 cd /mnt/alinea_vol/
 
-echo "downloading data from sentinel1 for zone : $ZONE"
+echo "downloading data from sentinel1 for zone : ${ZONE}"
 echo "Shapefile path: '${FOLDERPATH}/${SHAPEFILE_NAME}'"
-echo "Dates: $CAPTURE_DATE"
-python3 /root/ALINEA/scripts/download_sentinel1_from_extent.py $ZONE "${FOLDERPATH}/${SHAPEFILE_NAME}" $CAPTURE_DATE
+echo "Dates: ${CAPTURE_DATE}"
+python3 /root/ALINEA/scripts/download_sentinel1_from_extent.py $ZONE "/mnt/alinea_vol/${SHAPEFILE_NAME}" $CAPTURE_DATE
+#python3 /root/ALINEA/scripts/process_sentinel.py --folder-name "/mnt/alinea_vol/"
 
 echo "done."
