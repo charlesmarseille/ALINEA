2f7c3b0fd8a6ef182881bb89cb306b267dca7753
charlesmarseille <charles_marseille@hotmail.com>
2024-12-13 22:33:23 -0500

    corrected commands

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -10,7 +10,6 @@ cd /mnt/alinea_vol/
 echo "downloading data from sentinel1 for zone : ${ZONE}"
 echo "Shapefile path: "$FOLDER_PATH"/"$SHAPEFILE_NAME
 echo "Dates: ${CAPTURE_DATE}"
-# python3 /root/ALINEA/scripts/download_sentinel1_from_extent.py $ZONE "/mnt/alinea_vol/${SHAPEFILE_NAME}" $CAPTURE_DATE
-ls
-python3 /mnt/alinea_vol/process_sentinel.py --input-folder "/mnt/alinea_vol/data/${ZONE}/polygon0" --shapefile "/mnt/alinea_vol/$SHAPEFILE_NAME"
+python3 /root/ALINEA/scripts/download_sentinel1_from_extent.py $ZONE "/mnt/alinea_vol/${SHAPEFILE_NAME}" $CAPTURE_DATE
+python3 /root/alinea_vol/process_sentinel.py --input-folder "/mnt/alinea_vol/data/${ZONE}/polygon0" --shapefile "/mnt/alinea_vol/$SHAPEFILE_NAME"
 echo "done."
